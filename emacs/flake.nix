265d4684e3bac922e216ed54d96b2978dbb9ea93
Ralf Schmitt <ralf@systemexit.de>
2024-04-07 18:49:21 +0200

    Add a flake for emacs

---
+++ emacs/flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.emacs-overlay = {
+    url = "github:nix-community/emacs-overlay";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      emacs-overlay,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import emacs-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        emacs = pkgs.emacs-git;
+        emacs-packages =
+          epkgs: with epkgs; [
+            vterm
+            treesit-grammars.with-all-grammars
+            boxquote
+            crux
+            magit
+            eat
+          ];
+        emacs-with-packages = (pkgs.emacsPackagesFor emacs).emacsWithPackages (emacs-packages);
+      in
+      {
+        apps.default = {
+          type = "app";
+          program = "${emacs-with-packages}/bin/emacs";
+        };
+        defaultPackage = emacs-with-packages;
+        packages.default = emacs-with-packages;
+      }
+    );
+}
