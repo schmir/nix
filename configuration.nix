4a199d08a634711a79883d04fbaa693652f81508
Ralf Schmitt <ralf@systemexit.de>
2024-12-16 22:57:59 +0100

    Let me see the output of dmesg as normal user

--- configuration.nix
+++ configuration.nix
@@ -25,6 +25,10 @@
   networking.hostName = "${hostname}"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  boot.kernel.sysctl = {
+    "kernel.dmesg_restrict" = false;
+  };
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
