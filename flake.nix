de27ed72d2d502cf49be2dcc48f753bcf03a67bd
Ralf Schmitt <ralf@systemexit.de>
2025-01-12 23:45:19 +0100

    Use home-manager from sync-flake

--- flake.nix
+++ flake.nix
@@ -6,16 +6,12 @@
     nixpkgs-stable.follows = "sync/nixpkgs-stable";
     nixpkgs.follows = "sync/nixpkgs";
     flake-utils.follows = "sync/flake-utils";
+    home-manager.follows = "sync/home-manager";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
     # Specify the source of Home Manager and Nixpkgs.
     #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     schmir-emacs.url = "github:schmir/.emacs.d";
 
